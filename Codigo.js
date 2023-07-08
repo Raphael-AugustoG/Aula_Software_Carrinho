97a00f3dd85262499b24d5f7831df189468a5b95
Raphael-AugustoG <137107778+Raphael-AugustoG@users.noreply.github.com>
2023-07-08 01:09:09 -0300

    Update Codigo.js

--- Codigo.js
+++ Codigo.js
@@ -1,6 +1,6 @@
-
 let I = 0;
 let ult_erro = 0;
+let vel = 4000; //definição inicial de algumas variaveis
 
 function control(left_sensor, right_sensor, speed) {    
 
@@ -8,27 +8,27 @@ function control(left_sensor, right_sensor, speed) {
     var erro = right_sensor - left_sensor;
     
     var P = erro;
-    const Kp = 0.2;
+    const Kp = 0.43; // Variavel de controle proporcional
     
     
     I = I + erro;
-    const Ki = 0.04;
+    const Ki = 0.04; // Variavel de controle integral
  
     var D;
     D = erro - ult_erro;
     ult_erro = erro;
-    const Kd = 0.4;
+    const Kd = 0.41; // Variavel de controle derivativa
     
-    var vel = 1000; 
-    var angle = P*Kp + I*Ki + D*Kd; // PID dos links do Alex, estudo no outro arquivo
-
+    var angle = P*Kp + I*Ki + D*Kd; // Angulo que será mudado
+    console.log('vel:', vel);  //Acompanha velocidade pelo log
+    const AnglePar = 0.017; //Angulo de paramêtro para mudança nas velocidades
     
-//    if (angle > 0.0038 || angle < -0.0038) { Mudar a velocidade antes e depois da curva, dica do Piro
-//        vel = 2000;
-//    }
-//    else {
-//        vel = 5000;
-//    }
+    if ((angle > AnglePar && angle > 0) || (angle < -AnglePar && angle < 0) ){ //Condicional que muda a o enginetorque do carro baseado na sua angulação, mudança linear para o carro não sair voando. No caso 1500 até 4000.
+        vel = 1500;
+    }
+    else {
+        vel = 1500 + 2500*(1-((Math.abs(angle)/AnglePar)));
+    }
     
     
    
